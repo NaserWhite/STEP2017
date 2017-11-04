f13ad8cdfe14d1a603a4ef93f50f840530edfc08
NaserWhite <32420337+NaserWhite@users.noreply.github.com>
2017-11-04 14:55:11 -0400

    Add files via upload

---
+++ LIST_PRACTICE.py
@@ -0,0 +1,19 @@
+categories = [ "presidents"]
+
+categories.append("animes")
+categories.append("types of flowers")
+categories.append("types of birds")
+categories.append("spongebob character besides spongebob")
+categories.append("something halloween related")
+
+n = 0
+#while n < len(categories):
+    #print(categories[n])
+    #n = n + 1
+
+#for n in range (len(categories)):
+    #print(categories[n])
+z = 1
+while z < len(categories):
+    print(categories[z])
+    z = z + 2
