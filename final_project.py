899f5ebcdebac400a604d34ccd9324cb460453dd
NaserWhite <32420337+NaserWhite@users.noreply.github.com>
2017-12-16 13:44:39 -0500

    Add files via upload

--- final_project.py
+++ final_project.py
@@ -1,3 +1,4 @@
+
 username = input("Enter Your Name => ")
 print("Your name is " + username + " correct?")
 anwser = input("Answer yes or no => ")
@@ -12,13 +13,24 @@ if anwser == "yes":
 import random
 username = 0
 user_wins = 0
-for i in range (10):
-    random_num = random.randint (1, 10)
-    choice = int(input("Enter a number 1 through 10 => "))
-    if choice == random_num:
-        print("You gussed the right number")
-        user_wins = user_wins + 1
-    if choice < random_num:
-        print("Too high")
-    if choice > random_num:
-        print("Too low")
+loss = False
+random_num = random.randint(1, 10)
+while (loss == False):
+    for i in range(3):
+        choice = int(input("Enter a number 1 through 10 => "))
+        if choice == random_num:
+            print("You gussed the right number")
+            user_wins = user_wins + 1
+            break
+        if choice < random_num:
+            print("Too low")
+        if choice > random_num:
+            print("Too high")
+    if choice is not random_num:
+        print("You Lose")
+        print("Points:" + str(user_wins))
+        print("The random number was " + str(random_num))
+        loss = True
+    random_num = random.randint(1, 10)
+        
+
